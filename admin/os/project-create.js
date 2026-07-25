94ba9ebbb53bbd46d41ed467dd7943a5c806e985
LifeBookMom OS <yusun7749@gmail.com>
2026-07-25 19:07:04 +0900

    Phase 7 load Operations HQ dashboard

--- admin/os/project-create.js
+++ admin/os/project-create.js
@@ -123,6 +123,7 @@
     loadScript('/admin/os/next-task-engine.js', 'data-next-task-engine', () => Boolean(window.SavingioNextTask));
     loadScript('/admin/os/automation-controller.js', 'data-automation-controller', () => Boolean(window.SavingioAutomationController));
     loadScript('/admin/os/automation-qa-engine.js', 'data-automation-qa-engine', () => Boolean(window.SavingioAutomationQA));
+    loadScript('/admin/os/operations-hq.js', 'data-operations-hq', () => Boolean(window.SavingioOperationsHQ));
     loadScript('/admin/os/plugin-manifest.js', 'data-plugin-manifest', () => Boolean(window.SavingioPluginManifest));
     loadScript('/admin/os/plugin-manager.js', 'data-plugin-manager', () => Boolean(window.SavingioPluginManager));
     loadScript('/admin/os/plugin-security-engine.js', 'data-plugin-security-engine', () => Boolean(window.SavingioPluginSecurity));
